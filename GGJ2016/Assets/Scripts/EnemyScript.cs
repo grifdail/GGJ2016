914c0209b5cfad8addfea78b600c697c67c9daff
Florian Piette <f.piette@student.isartdigital.com>
2016-01-30 12:20:52 +0100

    Started the Enemy

---
+++ GGJ2016/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyScript : MonoBehaviour
+{      
+    private float _speed = 5f;
+    private float _timeSinceMoved = 0f;
+
+    private bool Fleeing = false;
+
+    // Use this for initialization
+    void Start()
+    {                          
+        StartCoroutine(Idle());
+    }
+
+    IEnumerator Idle()
+    {              
+        while (!Fleeing)
+        {
+
+        }
+
+        yield return null;
+    }
+}
