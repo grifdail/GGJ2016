3e819f096944250da962c2753e3c5e4ab57d3514
Florian Piette <f.piette@student.isartdigital.com>
2016-01-31 13:32:18 +0100

    Barre de progression

--- GGJ2016/Assets/Scripts/Gauge.cs
+++ GGJ2016/Assets/Scripts/Gauge.cs
@@ -9,21 +9,23 @@ public class Gauge : MonoBehaviour {
     public float value = 100;
     public float display = 0;
 
+    public GameObject Player;
     public RectTransform rectTransform;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         updateBar();
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        transform.GetChild(0).localPosition = new Vector3 (1.5f + 11 * (Player.transform.position.x/700f), transform.GetChild(0).localPosition.y, transform.GetChild(0).localPosition.z);
+
+    }
 
     public void updateBar()
     {
-        transform.localScale = new Vector3((value- min)/(max- min), 1, 1);
+        //transform.localScale = new Vector3((value- min)/(max- min), 1, 1);
     }
 }
