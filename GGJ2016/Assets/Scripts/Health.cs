1594e6f11801ee50377c494e8841c3c2ed4e690f
julien Berthou - grifdail <julien@grifdail.fr>
2016-01-31 07:42:49 +0100

    :poo:

---
+++ GGJ2016/Assets/Scripts/Health.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : Gauge{
+
+    public float hungerBySecond = 1;
+
+	// Use this for initialization
+	void Start () {
+        value = 100;
+        updateBar();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        value -= hungerBySecond * Time.deltaTime;
+        
+        if (value <= 0)
+        {
+            value = 0;
+            Debug.Log("Le petit il est mort");
+        }
+        updateBar();
+    }
+}
